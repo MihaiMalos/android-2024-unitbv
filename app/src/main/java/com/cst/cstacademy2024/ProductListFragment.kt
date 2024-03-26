44999e5377fb61e92d891f87ae6776b2fe700a1c
Maloș Mihai <malosmihai1@yahoo.com>
2024-03-26 15:59:42 +0200

    :-1:  :(

--- app/src/main/java/com/cst/cstacademy2024/ProductListFragment.kt
+++ app/src/main/java/com/cst/cstacademy2024/ProductListFragment.kt
@@ -10,11 +10,15 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.cst.cstacademy2024.adapters.ProductListAdapter
 import com.cst.cstacademy2024.helpers.extensions.logErrorMessage
+import com.cst.cstacademy2024.models.CartItemModel
 import com.cst.cstacademy2024.models.CategoryModel
 import com.cst.cstacademy2024.models.ProductModel
 
 class ProductListFragment : Fragment() {
-    val arguments: ProductListFragmentArgs by navArgs()
+    private val itemList = ArrayList<CartItemModel>()
+    private val adapter = ProductListAdapter(itemList)
+    private val arguments: ProductListFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,36 +33,16 @@ class ProductListFragment : Fragment() {
 
     private fun setupProductList() {
         val layoutManager = LinearLayoutManager(context)
-        val productList = listOf(
-            ProductModel(1, "Product 1", "Description 1"),
-            ProductModel(2, "Product 2", "Description 2"),
-            ProductModel(3, "Product 3", "Description 3"),
-            ProductModel(4, "Product 4", "Description 4"),
-            ProductModel(5, "Product 5", "Description 5"),
-            ProductModel(6, "Product 6", "Description 6"),
-            ProductModel(7, "Product 7", "Description 7"),
-            ProductModel(8, "Product 8", "Description 8"),
-            ProductModel(9, "Product 9", "Description 9"),
-            CategoryModel(99, "Category", "Description"),
-            ProductModel(10, "Product 10", "Description 10"),
-            ProductModel(11, "Product 11", "Description 1"),
-            ProductModel(12, "Product 12", "Description 2"),
-            ProductModel(13, "Product 13", "Description 3"),
-            CategoryModel(111, "Category", "Description"),
-            ProductModel(14, "Product 14", "Description 4"),
-            ProductModel(15, "Product 15", "Description 5"),
-            ProductModel(16, "Product 16", "Description 6"),
-            ProductModel(17, "Product 17", "Description 7"),
-            ProductModel(18, "Product 18", "Description 8"),
-            ProductModel(19, "Product 19", "Description 9"),
-            ProductModel(20, "Product 20", "Description 10"),
-        )
 
-        val adapter = ProductListAdapter(productList)
+
 
         view?.findViewById<RecyclerView>(R.id.product_list)?.apply {
             this.layoutManager = layoutManager
-            this.adapter = adapter
+            this.adapter = this@ProductListFragment.adapter
         }
+    }
+
+    private fun getCarItems() {
+
     }
 }
